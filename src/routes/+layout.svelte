c40838b2093504416f9a07ca2844968d68062be6
Pingu <nor@acorneroftheweb.com>
2022-12-05 15:59:16 +0100

    Website goes brrr (it has a base with css)

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,9 @@
+<script>
+  import "../app.css";
+</script>
+
+<html data-theme="black" lang="en"></html>
+
+<main class="flex flex-col max-w-lg mx-auto">
+  <slot />
+</main>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,16 @@
-<pre draggable="true">
+<div class="flex justify-center"><pre draggable="true">
        __ __  __              __
   ____/ // / / /____ _ _____ / /__
  / __  // /_/ // __ `// ___// //_/
 / /_/ // __  // /_/ // /__ / ,&lt
 \__,_//_/ /_/ \__,_/ \___//_/|_|
 </pre>
+</div>
+
+<div id="about">
+            <h2>About us</h2>
+            <p>dHack is the commitee responsible for managing and developing the digital systems of the D programme</p>
+
+            <p>Our services includes <a href="https://dtek.se">dtek.se</a> and various digital equipments in our student division premises.</p>
+            <p>Do you have any ideas for improvements or simply want to get in touch with us? Don't shoot us an email!</p>
+        </div>
